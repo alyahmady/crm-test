e15e718d3734f88e664649e86b29b8f8c531d010
alyahmady <better.aly.ahmady@gmail.com>
2023-05-01 22:27:39 +0200

    fix: create not-existing static files directories

--- crmtest/settings/paths.py
+++ crmtest/settings/paths.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from .base import BASE_DIR
 from .env import env
 
 STATIC_URL = "/static/"
 STATIC_ROOT = env.str("STATIC_ROOT", BASE_DIR / "static")
+
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+for directory in STATICFILES_DIRS:
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
