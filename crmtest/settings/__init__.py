0389e60ab0647681c08f3d1ad3d1fd0793265bb2
alyahmady <better.aly.ahmady@gmail.com>
2023-05-01 18:49:12 +0200

    feat: add package for `settings.py`

---
+++ crmtest/settings/__init__.py
@@ -0,0 +1,36 @@
+from .apps import INSTALLED_APPS
+from .auth import AUTH_PASSWORD_VALIDATORS
+from .base import (
+    BASE_DIR,
+    DEBUG,
+    SECRET_KEY,
+    ROOT_URLCONF,
+    WSGI_APPLICATION,
+    LANGUAGE_CODE,
+    TIME_ZONE,
+    USE_I18N,
+    USE_TZ,
+    DEFAULT_AUTO_FIELD,
+    AUTH_USER_MODEL,
+)
+from .cache import CACHES, REDIS_CONNECTION_URI
+from .database import DATABASES
+from .enums import *
+from .jwt import SIMPLE_JWT
+from .logging import LOGGING, LOGGER_NAME
+from .middleware import MIDDLEWARE
+from .paths import (
+    STATICFILES_DIRS,
+    STATICFILES_FINDERS,
+    STATIC_URL,
+    STATIC_ROOT,
+    MEDIA_URL,
+    MEDIA_ROOT,
+)
+from .rest_framework import (
+    REST_FRAMEWORK,
+    PAGINATION_MAX_PAGE_SIZE,
+    PAGINATION_PAGE_SIZE,
+)
+from .swagger import SPECTACULAR_SETTINGS
+from .template import TEMPLATES
