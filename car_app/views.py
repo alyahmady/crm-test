b19dc4a06b011c218a16458a6266949697106330
alyahmady <better.aly.ahmady@gmail.com>
2023-05-02 16:33:53 +0200

    feat: add pagination for `Car List` page

--- car_app/views.py
+++ car_app/views.py
@@ -3,14 +3,35 @@ from django.shortcuts import render
 
 from car_app.models import Car
 from crmtest.auth import active_user_required
+from crmtest.pagination import (
+    get_pagination_query_params,
+    paginate_queryset,
+    get_pagination_detail,
+)
 from rental_app.models import Rental
 
 
 @active_user_required
 def car_list_view(request):
+    limit, offset = get_pagination_query_params(request)
+
     in_progress_rental = Rental.objects.filter(car_id=OuterRef("pk"), is_returned=False)
-    cars = Car.objects.annotate(is_available=~Exists(in_progress_rental)).values(
-        "id", "name", "plate", "is_available"
+    cars_queryset = (
+        Car.objects.filter(is_active=True)
+        .annotate(is_available=~Exists(in_progress_rental))
+        .values("name", "plate", "body_style", "is_available")
     )
 
-    return render(request, "car_app/car_list.html", {"cars": cars})
+    paginated_cars, cars_count = paginate_queryset(cars_queryset, limit, offset)
+    pagination = get_pagination_detail(cars_count, limit, offset)
+
+    return render(
+        request,
+        "car_app/car_list.html",
+        {
+            "cars": paginated_cars,
+            "cars_count": cars_count,
+            "limit": limit,
+            "pagination": pagination,
+        },
+    )
